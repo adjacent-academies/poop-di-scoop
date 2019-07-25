4dd08eaaf214bb491f4840d197833c5d7effd2fd
Alexandra Romero <alromero@davidson.edu>
2019-07-25 14:52:59 -0700

    animal router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bathroom Mapper' });
+  console.log("hi there");
+  
+  res.render('index', { title: 'My Bathroom Mapper' });
 });
+
 
 module.exports = router;
